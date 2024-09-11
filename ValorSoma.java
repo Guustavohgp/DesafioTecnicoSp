5b3169d1583222c65138cdd12d5e718a3517d5bb
Guustavohgp <gustavo.henriquegpires@gmail.com>
2024-09-11 12:07:57 -0300

    add lógica ValorSoma

--- ValorSoma.java
+++ ValorSoma.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class ValorSoma {
+       public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int indece = 13, soma = 0, k = 1;
     
+        while (k<indece){
+            k = k+1; 
+            soma = soma + k;
+
+            System.out.println(soma);
+        }
+    } 
 }
